847c7cb7afe76e40ee123ef57f131bcf97c6be79
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-27 21:58:13 +0000

    area of rhombus

---
+++ area_of_rhombus.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by Donggeun Lim
+# Created on June 2020
+# This program calculates area of rhombus
+
+def main():
+    # this function calculates area of rhombus
+
+    # input
+    length = int(input("Enter length of the rectangle (mm): "))
+    width = int(input("Enter width of the rectangle (mm): "))
+
+    # process
+    area = length*width/2
+
+    # output
+    print("")
+    print("Area is {}mm²".format(area))
+
+
+if __name__ == "__main__":
+    main()
